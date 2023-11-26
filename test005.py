f654dbe092b9444c3fa9edd2fb232d2433a5b03f
omargtx2 <omargtx2@yahoo.es>
2023-11-25 22:01:08 -0500

    add five python file from local repo - change 2

--- test005.py
+++ test005.py
@@ -1,4 +1,4 @@
 print("add five python file from local repo")
 print("add five python file from local repo - change 1")
 
-print("add five python file from local repo - change 2" )
+print("add five python file from local repo - change 2")
